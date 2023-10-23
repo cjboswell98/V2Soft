b024803634e546b4b15f0caa37797e5672bf4051
Cedric Boswell Jr <116133429+cjboswell98@users.noreply.github.com>
2023-10-23 10:02:44 -0400

    Delete IdeaProjects/Rating2 directory

--- IdeaProjects/Rating2/src/main/java/com/product/rating/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.product.rating.repository;
-
-import com.product.rating.domain.Client;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ClientRepository extends MongoRepository<Client, String> {
-}
--- IdeaProjects/Rating2/src/main/java/com/product/rating/repository/JwtTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.product.rating.repository;
-
-import com.product.rating.model.JwtToken;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface JwtTokenRepository extends MongoRepository<JwtToken, String> {
-}
